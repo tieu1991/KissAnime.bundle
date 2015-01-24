aaa1b9805b0ecdbf7d3d470aa7263e13744d1891
tieu1991 <mathieu.leveille@uqtr.ca>
2015-01-23 20:57:58 -0500

    KissAnime.bundle

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,363 @@
+####################################################################################################
+
+PREFIX = "/video/KissAnime"
+
+NAME = "KissAnime"
+
+ICON_LIST = "icon-list.png"
+ICON_COVER = "icon-cover.png"
+ICON_SEARCH = "icon-search.png"
+ICON_QUEUE = "icon-queue.png"
+
+BASE_URL = "http://kissanime.com"
+
+RE_txha = Regex("txha = '.*';")
+
+#####################################################################################
+# Start
+#####################################################################################
+
+# This function is initially called by the PMS framework to initialize the plugin. This includes
+# setting up the Plugin static instance along with the displayed artwork.
+def Start():
+
+# Setup the default breadcrumb title for the plugin
+	ObjectContainer.title1 = NAME
+
+#####################################################################################
+# Main Menu
+#####################################################################################
+
+# This main function will setup the displayed items.
+# Initialize the plugin
+@handler(PREFIX, NAME)
+def MainMenu():
+    	oc = ObjectContainer()
+    	oc.add(DirectoryObject(key=Callback(Bookmark), title="Bookmark", thumb = R(ICON_LIST)))
+	oc.add(InputDirectoryObject(key=Callback(Search), title = "Search", prompt = "Search for anime?", thumb = R(ICON_SEARCH)))
+	return oc
+
+#####################################################################################
+# Search
+#####################################################################################
+
+@route(PREFIX + "/search")	
+def Search(query):
+		
+	oc = ObjectContainer(title1 = query)
+	
+	#setup the search request url
+	request_url = "http://kissanime.com/Search/Anime"
+	referer = "http://kissanime.com/"
+	values = {
+		'keyword':query
+		}
+
+	#do http request for search data
+	page = HTTP.Request(request_url, values = values, headers={'referer':referer})
+	page_data = HTML.ElementFromString(page)
+
+	list = page_data.xpath("//div[@id='leftside']//table[@class='listing']//tr")
+	list = list[2:]
+	
+	for each in list:
+
+		show_url = BASE_URL + each.xpath(".//td//a/@href")[0]
+		page_data = HTML.ElementFromURL(show_url)
+		show_title = show_url.rsplit('/',1)[1]
+		show_thumb = page_data.xpath("//div[@id='rightside']//img/@src")[0]
+		
+		oc.add(DirectoryObject(
+			key = Callback(PageEpisodes, show_title = show_title, show_url = show_url),
+			title = show_title,
+			thumb = Resource.ContentsOfURLWithFallback(url = show_thumb, fallback='icon-cover.png'),
+			)
+		)
+	
+	#check for zero results and display error
+	if len(oc) < 1:
+		Log ("No shows found! Check search query.")
+		return ObjectContainer(header="Error", message="Nothing found! Try something less specific.") 
+	
+	return oc
+
+
+#####################################################################################
+# Bookmark
+#####################################################################################
+
+@route(PREFIX + '/bookmark')
+def Bookmark():
+    	oc = ObjectContainer()
+
+	#setup the login request url
+	request_url = "http://kissanime.com/Login"
+	ad_bookmark = "http://kissanime.com/BookmarkList"
+	values = {
+		'username':Prefs["username"],
+		'password':Prefs["password"]
+		}
+
+	#do http request for search data
+	page = HTTP.Request(request_url, values = values)
+	
+	page_data = HTML.ElementFromURL(ad_bookmark)
+	data = HTML.StringFromElement(page_data)
+
+	list = page_data.xpath("//table[@class='listing']//tr")
+	list = list[2:]
+	
+	for each in list:
+
+		show_url = BASE_URL + each.xpath(".//td//a/@href")[0]
+		page_data = HTML.ElementFromURL(show_url)
+		show_title = show_url.rsplit('/',1)[1]
+		show_thumb = page_data.xpath("//div[@id='rightside']//img/@src")[0]
+		
+		oc.add(DirectoryObject(
+			key = Callback(PageEpisodes, show_title = show_title, show_url = show_url),
+			title = show_title,
+			thumb = Resource.ContentsOfURLWithFallback(url = show_thumb, fallback='icon-cover.png'),
+			)
+		)
+	
+	#check for zero results and display error
+	if len(oc) < 1:
+		Log ("No shows found! Check search query.")
+		return ObjectContainer(header="Error", message="Nothing found! Try something less specific.") 
+	
+     	return oc
+
+#####################################################################################
+# Page Episodes
+#####################################################################################
+ 
+@route(PREFIX + "/pageepisodes")
+def PageEpisodes(show_title, show_url):
+
+	oc = ObjectContainer(title1 = show_title)
+	page_data = HTML.ElementFromURL(show_url)
+	show_thumb = page_data.xpath("//div[@id='rightside']//img/@src")[0]
+	show_ep_count = len(page_data.xpath("//div[@id='leftside']//table[@class='listing']//tr")) - 2
+	#show_summary = page_data.xpath("//div[@id='leftside']/div[1]/div[2]/div[2]/p[6]/text()")[0]
+	eps_list = page_data.xpath("//div[@id='leftside']//table[@class='listing']//tr/td//a/@href")
+	eps_list.reverse()
+	
+	#set a start point and determine how many objects we will need
+	offset = 0
+	rotation = (show_ep_count - (show_ep_count % 20)) / 20
+
+	#add a directory object for every 20 episodes
+	while rotation > 0:
+	
+		start_ep  = offset
+		end_ep = offset + 20
+		start_ep_title = ((eps_list[(start_ep)].rsplit('-',1)[1]).rsplit('?id',1)[0]).strip()
+		end_ep_title = ((eps_list[(end_ep-1)].rsplit('-',1)[1]).rsplit('?id',1)[0]).strip()
+		
+		oc.add(DirectoryObject(
+			key = Callback(ListEpisodes, show_title = show_title, show_url = show_url, start_ep = start_ep, end_ep = end_ep),
+			title = "Episodes " + start_ep_title + " - " + end_ep_title,
+			thumb = Resource.ContentsOfURLWithFallback(url = show_thumb, fallback='icon-cover.png'),
+			#summary = show_summary
+			)
+		)
+		
+		offset += 20
+		rotation = rotation - 1
+	
+	#if total eps is divisible by 20, add bookmark link and return
+	if (show_ep_count % 20) == 0:	
+		
+		return oc
+	
+	#else create directory object for remaining eps
+	else:
+
+		start_ep = offset
+		end_ep = (offset + (show_ep_count % 20))
+		start_ep_title = ((eps_list[(start_ep)].rsplit('-',1)[1]).rsplit('?id',1)[0]).strip()
+		end_ep_title = ((eps_list[(end_ep-1)].rsplit('-',1)[1]).rsplit('?id',1)[0]).strip()
+		
+		oc.add(DirectoryObject(
+			key = Callback(ListEpisodes, show_title = show_title, show_url = show_url, start_ep = start_ep, end_ep = end_ep),
+			title = "Episodes " + start_ep_title + " - " + end_ep_title,
+			thumb = Resource.ContentsOfURLWithFallback(url = show_thumb, fallback='icon-cover.png'),
+			#summary = show_summary
+			)
+		)	
+		
+		return oc
+
+#####################################################################################
+# List Episodes
+#####################################################################################
+
+@route(PREFIX + "/listepisodes")	
+def ListEpisodes(show_title, show_url, start_ep, end_ep):
+
+	oc = ObjectContainer(title1 = show_title)
+	page_data = HTML.ElementFromURL(show_url)
+	eps_list = page_data.xpath("//div[@id='leftside']//table[@class='listing']//tr/td//a/@href")
+	eps_list.reverse()
+	
+	for each in eps_list[int(start_ep):int(end_ep)]:
+		ep_url = BASE_URL + each
+		ep_title = (each.rsplit('/',1)[1]).rsplit('?id',1)[0]
+		
+		if Prefs["quality"] == "Choose":
+		
+			oc.add(DirectoryObject(
+				key = Callback(Episodes, show_title = show_title, ep_title = ep_title, ep_url = ep_url), 
+				title = ep_title
+				)
+			)
+			
+		elif Prefs["quality"] == "1080p":
+		
+			page_data = HTML.ElementFromURL(ep_url)
+			data = HTML.StringFromElement(page_data)
+			
+			txha = RE_txha.search(data).group().split("= '",1)[1]
+			fmt_stream = txha.split("=",2)[2].split("&",1)[0]
+			fmt_stream = fmt_stream.replace("%252C",",").replace("%2f","/").replace("%3f","?").replace("%3d","=").replace("%26","&").replace("%3a",":").replace("https","http")
+			
+			
+			if fmt_stream.find("itag=37") > 0:
+				url = ep_url + "??" + fmt_stream.split("37%7C",1)[1].split("%2C",1)[0]
+				title = ep_title + " - 1920x1080"
+
+				oc.add(VideoClipObject(
+					url = url,
+					title = title
+					)
+				)
+					
+			elif fmt_stream.find("itag=22") > 0:
+				url = ep_url + "??" + fmt_stream.split("22%7C",1)[1].split("%2C",1)[0]
+				title = ep_title + " - 1280x720"
+				
+				oc.add(VideoClipObject(
+					url = url,
+					title = title
+					)
+				)
+					
+			elif fmt_stream.find("itag=18") > 0:
+				url = ep_url + "??" + fmt_stream.split("18%7C",1)[1]
+				title = ep_title + " - 640x360"
+					
+				oc.add(VideoClipObject(
+					url = url,
+					title = title
+					)
+				)
+				
+		elif Prefs["quality"] == "720p":
+		
+			page_data = HTML.ElementFromURL(ep_url)
+			data = HTML.StringFromElement(page_data)
+			
+			txha = RE_txha.search(data).group().split("= '",1)[1]
+			fmt_stream = txha.split("=",2)[2].split("&",1)[0]
+			fmt_stream = fmt_stream.replace("%252C",",").replace("%2f","/").replace("%3f","?").replace("%3d","=").replace("%26","&").replace("%3a",":").replace("https","http")
+					
+			if fmt_stream.find("itag=22") > 0:
+				url = ep_url + "??" + fmt_stream.split("22%7C",1)[1].split("%2C",1)[0]
+				title = ep_title + " - 1280x720"
+				
+				oc.add(VideoClipObject(
+					url = url,
+					title = title
+					)
+				)
+					
+			elif fmt_stream.find("itag=18") > 0:
+				url = ep_url + "??" + fmt_stream.split("18%7C",1)[1]
+				title = ep_title + " - 640x360"
+					
+				oc.add(VideoClipObject(
+					url = url,
+					title = title
+					)
+				)
+				
+		elif Prefs["quality"] == "360p":
+		
+			page_data = HTML.ElementFromURL(ep_url)
+			data = HTML.StringFromElement(page_data)
+			
+			txha = RE_txha.search(data).group().split("= '",1)[1]
+			fmt_stream = txha.split("=",2)[2].split("&",1)[0]
+			fmt_stream = fmt_stream.replace("%252C",",").replace("%2f","/").replace("%3f","?").replace("%3d","=").replace("%26","&").replace("%3a",":").replace("https","http")
+					
+			if fmt_stream.find("itag=18") > 0:
+				url = ep_url + "??" + fmt_stream.split("18%7C",1)[1]
+				title = ep_title + " - 640x360"
+					
+				oc.add(VideoClipObject(
+					url = url,
+					title = title
+					)
+				)		
+	return oc
+
+#####################################################################################
+# Episodes
+#####################################################################################
+
+@route(PREFIX + "/episodes")	
+def Episodes(show_title, ep_title, ep_url):
+
+	oc = ObjectContainer(title1=show_title)
+	
+	page_data = HTML.ElementFromURL(ep_url)
+	data = HTML.StringFromElement(page_data)
+	
+	txha = RE_txha.search(data).group().split("= '",1)[1]
+	fmt_stream = txha.split("=",2)[2].split("&",1)[0]
+	fmt_stream = fmt_stream.replace("%252C",",").replace("%2f","/").replace("%3f","?").replace("%3d","=").replace("%26","&").replace("%3a",":").replace("https","http")
+	
+	
+	if fmt_stream.find("itag=37") > 0:
+		url = ep_url + "??" + fmt_stream.split("37%7C",1)[1].split("%2C",1)[0]
+		title = "1920x1080"
+
+		oc.add(VideoClipObject(
+			url = url,
+			title = title
+			)
+		)
+			
+	if fmt_stream.find("itag=22") > 0:
+		url = ep_url + "??" + fmt_stream.split("22%7C",1)[1].split("%2C",1)[0]
+		title = "1280x720"
+		
+		oc.add(VideoClipObject(
+			url = url,
+			title = title
+			)
+		)
+			
+	if fmt_stream.find("itag=18") > 0:
+		url = ep_url + "??" + fmt_stream.split("18%7C",1)[1]
+		title = "640x360"
+			
+		oc.add(VideoClipObject(
+			url = url,
+			title = title
+			)
+		)
+	
+	
+	return oc
+	
+#####################################################################################
+# Format
+#####################################################################################4
+@route(PREFIX + "/format")
+def fmt():
+
+	oc = ObjectContainer()
+	
+	return oc
